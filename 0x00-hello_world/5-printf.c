c8679814e787d6c0d9968206929c4f00e36edca8
olamiji <abdulmalikayinde75@gmail.com>
2023-07-15 15:57:20 -0700

    error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using printf function
+ * main - A program that prints a line using printf function
  * Return 0 (success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
